81d1e34f192c6351d707736b947125b2f4d0e3b9
Zeereoreo <wltjrcjf12@naver.com>
2023-03-07 14:27:56 +0900

    session

--- fe-sprint-auth-session/server/index.js
+++ fe-sprint-auth-session/server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const logger = require('morgan');
-const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const fs = require('fs');
 const https = require('https');
 const controllers = require('./controllers');
@@ -12,22 +12,38 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 const HTTPS_PORT = process.env.HTTPS_PORT || 4000;
 
+// express-session 라이브러리를 이용해 쿠키를 설정할 수 있습니다.
+app.use(
+  session({
+    secret: '@codestates',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      domain: 'localhost',
+      path: '/',
+      httpOnly: true,
+
+      // express-session 미들웨어를 사용하면, 쿠키의 secure 옵션이 true일 경우,
+      // Domain이 localhost여도 https 서버를 실행해야 제대로 작동합니다.
+      // express-session을 사용하면서 http 서버에서 쿠키를 사용하려면 samsSite, secure 옵션을 작성하지 않으면 됩니다.
+      // (단, 실제 배포 단계에서는 https 서버 사용과 동시에 sameSite, secure 옵션이 설정되어 있어야 안전합니다.)
+
+      // sameSite: 'none',
+      // secure: true,
+    },
+  })
+);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 
 const corsOptions = {
-
-	// client는 http://localhost:3000 을 이용하게 됩니다.
-  origin: "http://localhost:3000",
-
-	// cookie는 인증 정보를 포함하는 경우가 많으므로 credentials도 설정해줍니다.
-  credentials: true,
-
-	// 허용할 메소드를 배열에 담아서 작성해줍니다.
-  methods: ['GET', 'POST', 'OPTION']
-
+  /* TODO: CORS 설정이 필요합니다. 클라이언트가 어떤 origin인지에 따라 달리 설정할 수 있습니다.
+   * 메서드는 GET, POST, OPTIONS를 허용합니다.
+   */
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'OPTIONS'],
+    credentials: true,
 };
 app.use(cors(corsOptions));
 
@@ -37,7 +53,6 @@ app.get('/userinfo', controllers.userInfo);
 
 // (Optional) https 서버를 실행합니다.
 // 아래 코드는 인증서 파일이 존재하지 않는 경우에는 http 서버를, 존재하는 경우에는 https 서버를 실행합니다.
-// 서버와 클라이언트의 Domain이 localhost이기 때문에, 쿠키를 사용하기 위해서 https 서버가 꼭 필요한 것은 아닙니다.
 // https 프로토콜을 사용하는 서버를 실행해보고 싶다면, mkcert를 사용해 인증서 파일을 server 폴더 안에 만들어주세요.
 let server;
 if (fs.existsSync('./key.pem') && fs.existsSync('./cert.pem')) {
