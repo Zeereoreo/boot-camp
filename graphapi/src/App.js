a57ea89781e0b6bd68fe31b427b6a2e3d4737b44
Zeereoreo <wltjrcjf12@naver.com>
2023-03-28 15:03:30 +0900

    graph ql 헤드 및 콘솔 삭제

---
+++ graphapi/src/App.js
@@ -0,0 +1,66 @@
+import './App.css';
+import { graphql } from "@octokit/graphql";
+import { useEffect, useState } from 'react';
+
+let token = `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`
+
+const graphqlWithAuth = graphql.defaults({
+  headers: {
+    authorization: `token ${token}`,
+  },
+});
+
+async function Graphrepo  (){
+    const { repository, viewer } = await graphqlWithAuth(
+      `
+      {
+        repository(owner: "codestates-seb", name: "agora-states-fe") {
+          discussions(first : 3) {
+            edges {
+              node {
+                id
+                title
+                createdAt
+                url
+                author {
+                  login
+                  avatarUrl
+                }
+             }
+            }
+          }
+        }
+        viewer {
+          login 
+        }
+      }
+      `
+    );
+    return {repository,viewer}
+}
+  
+function App() {
+  const [repository, setRpository] = useState({});
+  const [viewer, setViewer] = useState({})
+
+  useEffect(()=>{
+    Graphrepo()
+    .then((data)=>{ 
+      setRpository(data.repository)
+      setViewer(data.viewer)
+    })
+    .catch((error)=>{
+      console.log(Error, error)
+    }) 
+  },[])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button >콘솔</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
