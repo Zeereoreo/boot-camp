1e4d3065653c6c2c38d3b4d965133f2159672f2f
Zeereoreo <wltjrcjf12@naver.com>
2023-03-30 16:37:47 +0900

    todoapp 헤더완료

--- To-do-App/react-to-do-app/src/components/Header.js
+++ To-do-App/react-to-do-app/src/components/Header.js
@@ -1,18 +1,18 @@
 import React from "react";
-import { ImMenu } from "react-icons/im";
+import { ImMenu , ImCalendar} from "react-icons/im";
 import styled from "styled-components";
-import styles from 'styled-components';
 
 const Header = () => {
 const HeaderLine = styled.div`
     display : flex;
-    justify-content : flex-start;
+    justify-content : space-around;
     align-items: center; 
 `;    
 return (
     <HeaderLine>   
-        <ImMenu />
+        <ImMenu className="" />
         <h1>매일 작심삼일</h1>
+        <ImCalendar className=""/>
     </HeaderLine>
     );
 }
