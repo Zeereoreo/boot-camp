dd0a927ae68172fd31f800ea7ceb6487f13e8ddb
Zeereoreo <wltjrcjf12@naver.com>
2023-04-11 17:25:16 +0900

    todoapp template 설정

--- To-do-App/react-to-do-app/src/components/Template.js
+++ To-do-App/react-to-do-app/src/components/Template.js
@@ -3,6 +3,15 @@ import styled from "styled-components";
 import Todoinsert from './Todoinsert';
 import Showtodolist from './Showtodolist';
 
+const TodoTemplateWrap = styled.div`
+    width: 512px;
+    height: 768px;
+
+    position: relative;
+    background-color: gray;
+    border-radius: 13px;
+`
+
 
 const Template = (todoData) => {
  
@@ -10,7 +19,7 @@ const Template = (todoData) => {
 return (
     <div className="todolist-template-wraper">
         <Todoinsert></Todoinsert>
-        <Showtodolist todoData={todoData}></Showtodolist>
+        <Showtodolist></Showtodolist>
     </div>
 )
 }
